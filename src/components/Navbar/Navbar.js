ea0bd9d4284d3ae02235f5dae7dff0c03dd26b8b
alielbaamrani <ali.elbaamrani21@gmail.com>
2023-05-26 14:41:55 +0200

    p11v.1.0.0

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,9 +7,7 @@ export default function Navbar() {
   return (
     <nav>
       <Link className="logo" to="/">
-        <img  src={logo} width={"100%"}
-           height={"100%"}
-        />
+        <img src={logo} width={"100%"} height={"100%"} />
       </Link>
 
       <div className="link">
